2700ea4d1d1d0dd3d60c48cd7bfe885c2ef44c35
Fede <fms0997@gmail.com>
2023-08-16 19:35:17 -0300

    Actualizacion

--- Graficos para Epi.R
+++ Graficos para Epi.R
@@ -14,25 +14,65 @@ library(tmap)
 library(leaflet)
 library(DT)
 
-##leo la tablas
-datos_respiratorias <- 
-  read_excel("RMD/RMD003_Analisis/datos/informacion-publica-respiratorias-nacional-hasta-20230706.xlsx")
+# Define la lista de URLs a descargar
+urls <- c(
+  #1
+  "http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/f4096f8b-1692-4d5f-a6d8-09cae47931a4/download/vigilancia-respiratorias-agudas-2018-hasta-20200106.csv",
+  #2
+  "http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/9a62f016-ba4e-4366-a883-22e9dc785d39/download/informacion-publica-respiratorias-nacional-hasta-20180626.csv",
+  #3
+  "http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/0c29cc75-d81e-4f87-9257-4c30c24bfe55/download/vigilancia-de-infecciones-respiratorias-agudas-20181228.csv",
+  #4
+  "http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/f2998314-9087-4641-aec7-f2f67c9ba865/download/informacion-publica-respiratorias-nacional-hasta-20220905.xls",
+  #5
+  "http://datos.salud.gob.ar/dataset/c553d917-36f4-4063-ac02-1686a9120e1c/resource/37feeed4-dfcd-4f39-a403-93a6b4bd90d2/download/informacion-publica-respiratorias-nacional-hasta-20230706.xlsx"
+  # Agrega más URLs según sea necesario
+)
 
+# Define los nombres de los archivos (pueden ser diferentes a los nombres originales)
+nombres_archivos <- c(
+  "vigilancia-respiratorias-agudas-2018-hasta-20200106.csv",
+  "informacion-publica-respiratorias-nacional-hasta-20180626.csv",
+  "vigilancia-de-infecciones-respiratorias-agudas-20181228.csv",
+  "informacion-publica-respiratorias-nacional-hasta-20220905.xls",
+  "informacion-publica-respiratorias-nacional-hasta-20230706.xlsx"
+  # Agrega más nombres de archivos según sea necesario
+)
 
-##para otros años
-datos_respiratorias2 <- read_excel("RMD/RMD003_Analisis/datos/informacion-publica-respiratorias-nacional-hasta-20220905.xlsx")
+# Carpeta de destino
+carpeta_destino <- "~/curso_shiny_Fede/Datos"
 
-datos_eti <- datos_respiratorias %>%
-  filter(evento_nombre == "Enfermedad tipo influenza (ETI)") %>%
-  group_by(provincia_nombre, provincia_id, año, semanas_epidemiologicas) %>%
-  summarise(conteo = sum(cantidad_casos))
+# Verifica y descarga los archivos desde las URLs
+for (i in 1:length(urls)) {
+  # Ruta completa del archivo en la carpeta /datos
+  ruta_completa <- file.path(carpeta_destino, nombres_archivos[i])
+  
+  # Verifica si el archivo ya existe en la carpeta
+  if (!file.exists(ruta_completa)) {
+    # Si el archivo no existe, descárgalo desde la URL
+    download.file(urls[i], destfile = ruta_completa, mode = "wb")
+    cat("Archivo descargado y guardado en:", ruta_completa, "\n")
+  } else {
+    cat("El archivo ya existe en la carpeta:", ruta_completa, "\n")
+  }
+}
 
-datos_eti2 <- datos_respiratorias2 %>%
-  filter(evento_nombre == "Enfermedad tipo influenza (ETI)",
-         año != 2022) %>%
-  group_by(provincia_nombre, provincia_id, año, semanas_epidemiologicas) %>%
-  summarise(conteo = sum(cantidad_casos))
+# Leer los archivos y asignar a objetos en R
+datos <- list()  # Lista para almacenar los datos
 
-datos_eti <- rbind(datos_eti, datos_eti2)
-DT::datatable(head(datos_eti))
+for (i in 1:length(nombres_archivos)) {
+  ruta_completa <- file.path(carpeta_destino, nombres_archivos[i])
+  extension <- tools::file_ext(nombres_archivos[i])
+  
+  if (extension == "csv") {
+    datos[[i]] <- read.csv(ruta_completa, encoding = "UTF-8")
+  } else if (extension == "xlsx" | extension == "xls") {
+    datos[[i]] <- read_excel(ruta_completa)
+  }
+}
 
+# Asignar a objetos con nombres específicos
+for (i in 1:length(nombres_archivos)) {
+  assign(paste0("datos", i), datos[[i]])
+  cat("Datos", i, "cargados en objeto:", paste0("datos", i), "\n")
+}
